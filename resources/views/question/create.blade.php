3b8ac468ff271f124622d782b53192af2e64f67b
MingWei <w2040w@outlook.com>
2020-02-20 21:41:59 +0800

    chmod all file

--- resources/views/question/create.blade.php
+++ resources/views/question/create.blade.php
@@ -5,13 +5,13 @@
 <form action="/register" method="post">
 @csrf
 <div class='form-group'>
-<label for='title'>question title</label>
-<input type='text' class='form-control' id='title' name='title' />
+    <label for='title'>question title</label>
+    <input type='text' class='form-control' id='title' name='title' />
 </div>
 <div class='form-group'>
-<label for='content'>question content</label>
-<textarea class='form-control' id='content' name='content' >
-</div>
-<button type="submit" class='btn btn-primary'>submit question</button>
+    <label for='content'>question content</label>
+    <textarea class='form-control' id='content' name='content' >
+    </div>
+    <button type="submit" class='btn btn-primary'>submit question</button>
 </form>
 @endsection
